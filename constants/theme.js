9f298f7f594116a8c1b2fffa23ee985a52a487da
Mahdi <mahdiaghamohammadi28@gmail.com>
2025-09-18 21:24:22 +0330

    Add theme constants for color palette in theme.js

---
+++ constants/theme.js
@@ -0,0 +1,11 @@
+export const COLORS = {
+  primary: "#028174",
+  secondary: "#0AB68B",
+  accent1: "#92DE8B",
+  accent2: "#FFE3B3",
+  bg: "#faf0dd",
+  error: "#E65C4F",
+  white: "#fff",
+  black: "#282828",
+  border: "#2a2a2a",
+};
