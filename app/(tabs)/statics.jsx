6fd43799aab51bc1e17cd3d63ae915dc8ccfefc1
Mahdi <mahdiaghamohammadi28@gmail.com>
2025-09-19 17:26:50 +0330

    Add new tab components: Categories, Settings, Statics, and Transactions for improved navigation and organization

---
+++ app/(tabs)/statics.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Statics() {
+  return (
+    <View>
+      <Text>Statics</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ app/(tabs)/transactions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Transactions() {
+  return (
+    <View>
+      <Text>Transactions</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
