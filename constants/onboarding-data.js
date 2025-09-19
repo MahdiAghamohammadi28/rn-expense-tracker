40e8f281acb255229ba46494620907a9f9ef1640
Mahdi <mahdiaghamohammadi28@gmail.com>
2025-09-19 14:18:09 +0330

    Add onboarding data constants

---
+++ constants/onboarding-data.js
@@ -0,0 +1,26 @@
+import expenseImg from "../assets/images/expense.png";
+import growthImg from "../assets/images/growth.png";
+import budgeImg from "../assets/images/money-bag.png";
+
+export const ONBOARDING_DATA = [
+  {
+    id: "d1",
+    title: "Track Your Spending",
+    description:
+      "Easily log your daily expenses and income to keep your finances organized.",
+    image: expenseImg,
+  },
+  {
+    id: "d2",
+    title: "Stay Within Budget",
+    description:
+      "Set category-based budgets and monitor progress with smart insights.",
+    image: budgeImg,
+  },
+  {
+    id: "d3",
+    title: "Understand Your Finances",
+    description: "Get clear charts and analytics to see where your money goes.",
+    image: growthImg,
+  },
+];
