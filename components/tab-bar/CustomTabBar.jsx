405af4e4b9c664de5282089a4fddce704e634938
Mahdi <mahdiaghamohammadi28@gmail.com>
2025-09-19 17:26:12 +0330

    Add CustomTabBar component with animated tab transitions and dynamic layout handling

---
+++ components/tab-bar/CustomTabBar.jsx
@@ -0,0 +1,112 @@
+import TabBarButton from "@/components/tab-bar/TabBarButton";
+import { COLORS } from "@/constants/theme";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+
+export default function CustomTabBar({ state, descriptors, navigation }) {
+  const [dimensions, setDimensions] = useState({ height: 20, width: 100 });
+
+  const buttonWidth = dimensions.width / state.routes.length;
+
+  const onTabBarLayout = (e) => {
+    setDimensions({
+      height: e.nativeEvent.layout.height,
+      width: e.nativeEvent.layout.width,
+    });
+  };
+
+  const tabPositionX = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: tabPositionX.value }],
+    };
+  });
+
+  return (
+    <View onLayout={onTabBarLayout} style={styles.container}>
+      <Animated.View
+        style={[
+          animatedStyle,
+          {
+            position: "absolute",
+            left: 0,
+            backgroundColor: COLORS.secondary,
+            borderRadius: 1000,
+            marginHorizontal: 12,
+            height: dimensions.height - 15,
+            width: buttonWidth - 25,
+          },
+        ]}
+      />
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          tabPositionX.value = withSpring(buttonWidth * index, {
+            duration: 500,
+          });
+
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <TabBarButton
+            key={route.name}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            isFocused={isFocused}
+            label={label}
+            routeName={route.name}
+            color={isFocused ? COLORS.primary : "#969696"}
+          />
+        );
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 5,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 10,
+    borderRadius: 1000,
+    elevation: 3,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: COLORS.border,
+    marginHorizontal: 10,
+  },
+});
