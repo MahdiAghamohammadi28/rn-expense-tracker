071e5d60ab997ab49d460a76b553b81bcdcbd6a0
Mahdi <mahdiaghamohammadi28@gmail.com>
2025-09-19 17:27:05 +0330

    Add new tab screens for Transactions, Categories, Statics, and Settings, and integrate CustomTabBar for enhanced navigation experience

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,45 @@
+import CustomTabBar from "@/components/tab-bar/CustomTabBar";
 import { Tabs } from "expo-router";
 import React from "react";
 
 export default function TabLayout() {
   return (
     <Tabs
+      tabBar={(props) => <CustomTabBar {...props} />}
       screenOptions={{
         headerShown: false,
+        tabBarInactiveTintColor: "#c4c4c4",
+        tabBarHideOnKeyboard: true,
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
+        }}
+      />
+      <Tabs.Screen
+        name="transactions"
+        options={{
+          title: "Transactions",
+        }}
+      />
+      <Tabs.Screen
+        name="categories"
+        options={{
+          title: "Categories",
+        }}
+      />
+      <Tabs.Screen
+        name="statics"
+        options={{
+          title: "Statics",
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
         }}
       />
     </Tabs>
